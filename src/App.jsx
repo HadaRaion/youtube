3be6071f93401cb83c204ed962de5ad755f93b0c
hada <hadathing@gmail.com>
2023-04-11 15:39:30 -0700

    init project.

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+	return (
+		<div className="App">
+			<h1 className="text-4xl font-bold underline text-9xl">Hello world!</h1>
+		</div>
+	);
+}
+
+export default App;
