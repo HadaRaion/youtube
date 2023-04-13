c97b89c3ad30751e2995fe9acbae96c6f554beb3
hada <hadathing@gmail.com>
2023-04-13 15:29:25 -0700

    Style body

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,12 @@
 import { Outlet } from 'react-router-dom';
 import Header from './components/Header';
-import './App.css';
 
 function App() {
 	return (
-		<div className="min-h-screen flex flex-col items-center bg-gray-700 text-white">
+		<>
 			<Header />
-			<div id="detail">
-				<Outlet />
-			</div>
-		</div>
+			<Outlet />
+		</>
 	);
 }
 
